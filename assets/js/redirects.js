aea22a0474ed0e2f308704a27697b8832d8f6dd1
Razerek <kkilikepie@gmail.com>
2021-07-29 21:40:08 -0700

    Add files via upload

---
+++ assets/js/redirects.js
@@ -0,0 +1,16 @@
+function main()
+{
+	window.location.replace("../main/index.html")
+}
+function pixto()
+{
+	window.location.replace("../picto/index.html")
+}
+function dream()
+{
+	window.location.replace("../dream/index.html")
+}
+function notFound()
+{
+	window.location.replace("../deadend/index.html")
+}
